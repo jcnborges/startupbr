b6fc3623e5216ca868b3af649d1ecad41cb19a79
Júlio César Borges da Silveira Nardelli <jcn.borges@gmail.com>
2020-04-23 20:25:01 -0300

    Coleta de hoje

--- src/datamining/get_georef_translate_categoria.py
+++ src/datamining/get_georef_translate_categoria.py
@@ -3,9 +3,9 @@ import csv
 import googlemaps
 from google.cloud import translate_v2 as translate
 
-CSV_FILE = 'csv/dimorganization_test.csv'
+CSV_FILE = 'csv/dimorganization.csv'
 CSV_OUT = 'csv/out.csv'
-API_KEI = 'AIzaSyCPDnOfBz4rH1xfBTdwof7m1of2nuA3CiU'
+API_KEI = 'AIzaSyBTZaKCSfAbbltig9-y2gQPHAurjvcK4yA'
 API_LANGUAGE = "pt"
         
 def localizar(gmaps, nomeLocalidade):
@@ -33,39 +33,63 @@ def traduzir_pt(gtrans, origem):
         print(str(e))
         return None
 
-with open(CSV_FILE, newline = '') as csvfile:
-    gmaps = googlemaps.Client(key = API_KEI)
-    gtrans = translate.Client()    
-    spamreader = csv.reader(csvfile, delimiter = ';')
-    result = []
-    colunas = {"id_empresa;nome_empresa;cidade_empresa;categoria_empresa;latitude;longitude;endereco;categoria_pt"}
-    next(spamreader, None)
-    for row in spamreader:
-        
-        # enriquecer geo_ref
-        
-        id_empresa = row[0]
-        nome_empresa = row[1]
-        cidade_empresa = row[2]
-        categoria_empresa = row[3]
-        latitude = ""
-        longitude = ""
-        endereco = ""
-        categoria_pt = ""
-        
-        gplace = localizar(gmaps, "{0} {1}".format(nome_empresa, cidade_empresa))
-        if gplace != None:
-            latitude = gplace["geometry"]["location"]["lat"]
-            longitude = gplace["geometry"]["location"]["lng"]
-            endereco = gplace["formatted_address"]       
-        
-        # translate categorias
-        
-        categoria_empresa_pt = traduzir_pt(gtrans, categoria_empresa)
-        result.append("{0};{1};{2};{3};{4};{5};{6}:{7}".format(id_empresa, nome_empresa, cidade_empresa, categoria_empresa, latitude, longitude, endereco, categoria_empresa_pt))    
-
-with open(CSV_OUT, mode = 'w', newline = '') as csvfile:
-    spamwriter = csv.writer(csvfile)
-    spamwriter.writerow(colunas)
-    for r in result:
-        spamwriter.writerow(r)
+result = []
+try:
+    with open(CSV_FILE, newline = '') as csvfile:
+        gmaps = googlemaps.Client(key = API_KEI)
+        gtrans = translate.Client()    
+        spamreader = csv.reader(csvfile, delimiter = ';')
+    
+        colunas = ["id_empresa", "nome_empresa", "cidade_empresa", "categoria_empresa", "nome_google_place", "latitude", "longitude", "endereco", "endereco_detalhe", "categoria_empresa_pt"]
+        next(spamreader, None)
+        for row in spamreader:
+            
+            # enriquecer geo_ref
+            
+            id_empresa = row[0]
+            nome_empresa = row[1]
+            cidade_empresa = row[2]
+            categoria_empresa = row[3]
+            nome_google_place = ""
+            latitude = ""
+            longitude = ""
+            endereco = ""
+            endereco_detalhe = ""
+            categoria_empresa_pt = ""
+            
+            gplace = localizar(gmaps, "{0} {1}".format(nome_empresa, cidade_empresa))
+            if gplace == None:
+                gplace = localizar(gmaps, nome_empresa)
+            if gplace != None:
+                nome_google_place = gplace["name"]
+                latitude = gplace["geometry"]["location"]["lat"]
+                longitude = gplace["geometry"]["location"]["lng"]
+                endereco = gplace["formatted_address"] 
+                try:
+                    endereco_detalhe = gplace["plus_code"]["compound_code"]
+                except Exception as e:
+                    print(e)
+            
+            # translate categorias
+            
+            categoria_empresa_pt = traduzir_pt(gtrans, categoria_empresa)
+            
+            r = []
+            r.append(id_empresa)
+            r.append(nome_empresa)
+            r.append(cidade_empresa)
+            r.append(categoria_empresa)
+            r.append(nome_google_place)
+            r.append(latitude)
+            r.append(longitude)
+            r.append(endereco)
+            r.append(endereco_detalhe)
+            r.append(categoria_empresa_pt)
+            
+            result.append(r)    
+finally:
+    with open(CSV_OUT, mode = 'w', newline = '') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';')
+        spamwriter.writerow(colunas)
+        for r in result:
+            spamwriter.writerow(r)
